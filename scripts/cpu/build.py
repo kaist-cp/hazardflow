b68605aad6f670b9cf1141aa016f3c08ecb91c9c
Minseong Jang <minseong.jang@kaist.ac.kr>
2024-09-03 16:10:50 +0900

    Update CPU build script

--- scripts/cpu/build.py
+++ scripts/cpu/build.py
@@ -12,7 +12,7 @@ def build_core():
     Generate Verilog and Simulator
     """
     logger.info(f"Start generating Verilog code and building Emulator")
-    subprocess.run(
+    virgen = subprocess.run(
         [
             "cargo",
             "run",
@@ -20,8 +20,8 @@ def build_core():
             "--",
             "--target",
             "core",
-            "--wire-cache",
             "--deadcode",
+            "--wire-cache",
             "--merge",
             "--system-task",
         ],
@@ -29,7 +29,12 @@ def build_core():
         stderr=subprocess.DEVNULL,
         cwd=hazardflow_dir,
     )
-    logger.info(f"[HAZARDFLOW] Verilog code compiled at {hazardflow_dir}/build/core")
+
+    if virgen.returncode != 0:
+        logger.error("`cargo run --release -- --target core --deadcode --wire-cache --merge --system-task` failed.")
+        exit(1)
+
+    logger.info(f"Verilog code compiled at {hazardflow_dir}/build/core")
 
     subprocess.run(f"cp {hazardflow_dir}/build/core/*.v {sodor_dir}/vsrc", shell=True)
     subprocess.run(["make", "emulator-debug"], cwd=f"{sodor_dir}/emulator/rv32_5stage_hf")
