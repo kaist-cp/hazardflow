54b7bd76f0a28a61529389dd9b925955b8bb5a52
Minseong Jang <minseong.jang@kaist.ac.kr>
2024-09-05 12:27:18 +0900

    Update submission script

--- scripts/make_submission.sh
+++ scripts/make_submission.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 # Clears the previous submissions.
-rm -rf hw2.zip
+rm -rf hw2.zip hw3.zip
 
 # Creates new submissions.
 zip hw2.zip -j hazardflow-designs/src/cpu/fetch.rs hazardflow-designs/src/cpu/decode.rs hazardflow-designs/src/cpu/exe.rs hazardflow-designs/src/cpu/branch_predictor/bht.rs hazardflow-designs/src/cpu/branch_predictor/btb.rs
+zip hw3.zip -j hazardflow-designs/src/cpu/decode.rs hazardflow-designs/src/cpu/exe.rs hazardflow-designs/src/cpu/alu.rs hazardflow-designs/src/cpu/riscv_isa.rs
