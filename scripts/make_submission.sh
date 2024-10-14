4c50fbf4658a36d0a4f97a90635b3a815ac3a011
Minseong Jang <minseong.jang@kaist.ac.kr>
2024-10-14 14:39:26 +0900

    Update hw3 submission script

--- scripts/make_submission.sh
+++ scripts/make_submission.sh
@@ -5,4 +5,4 @@ rm -rf hw2.zip hw3.zip
 
 # Creates new submissions.
 zip hw2.zip -j hazardflow-designs/src/cpu/fetch.rs hazardflow-designs/src/cpu/decode.rs hazardflow-designs/src/cpu/exe.rs hazardflow-designs/src/cpu/branch_predictor/bht.rs hazardflow-designs/src/cpu/branch_predictor/btb.rs
-zip hw3.zip -j hazardflow-designs/src/cpu/exe.rs hazardflow-designs/src/cpu/riscv_isa.rs
+zip hw3.zip -j hazardflow-designs/src/cpu/fetch.rs hazardflow-designs/src/cpu/decode.rs hazardflow-designs/src/cpu/exe.rs hazardflow-designs/src/cpu/branch_predictor/bht.rs hazardflow-designs/src/cpu/branch_predictor/btb.rs hazardflow-designs/src/cpu/riscv_isa.rs
