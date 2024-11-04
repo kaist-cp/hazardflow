9cf6d01d4eb8e479d0802356b19b3cfdbe06dd53
Minseong Jang <minseong.jang@kaist.ac.kr>
2024-11-04 23:47:45 +0900

    Update submission scripts

--- scripts/make_submission.sh
+++ scripts/make_submission.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # Clears the previous submissions.
-rm -rf hw2.zip hw3.zip
+rm -rf hw2.zip hw3.zip hw5.zip
 
 # Creates new submissions.
-zip hw2.zip -j hazardflow-designs/src/cpu/fetch.rs hazardflow-designs/src/cpu/decode.rs hazardflow-designs/src/cpu/exe.rs hazardflow-designs/src/cpu/branch_predictor/bht.rs hazardflow-designs/src/cpu/branch_predictor/btb.rs
-zip hw3.zip -j hazardflow-designs/src/cpu/fetch.rs hazardflow-designs/src/cpu/decode.rs hazardflow-designs/src/cpu/exe.rs hazardflow-designs/src/cpu/branch_predictor/bht.rs hazardflow-designs/src/cpu/branch_predictor/btb.rs hazardflow-designs/src/cpu/riscv_isa.rs
+zip hw2.zip -j hazardflow-designs/src/cpu/fetch.rs hazardflow-designs/src/cpu/decode.rs hazardflow-designs/src/cpu/exe.rs hazardflow-designs/src/cpu/branch_predictor/bht.rs hazardflow-designs/src/cpu/branch_predictor/btb.rs hazardflow-designs/src/cpu/branch_predictor/mod.rs
+zip hw3.zip -j hazardflow-designs/src/cpu/fetch.rs hazardflow-designs/src/cpu/decode.rs hazardflow-designs/src/cpu/exe.rs hazardflow-designs/src/cpu/branch_predictor/bht.rs hazardflow-designs/src/cpu/branch_predictor/btb.rs hazardflow-designs/src/cpu/branch_predictor/mod.rs hazardflow-designs/src/cpu/riscv_isa.rs
 zip hw5.zip -j hazardflow-designs/src/gemmini/execute/systolic_array/mesh.rs hazardflow-designs/src/gemmini/execute/systolic_array/transposer.rs
